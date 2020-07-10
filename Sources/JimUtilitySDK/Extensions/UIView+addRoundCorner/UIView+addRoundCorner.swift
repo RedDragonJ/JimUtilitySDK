5caa78b79a2847a9451f404859bb9591a027d29e
AsianJim <jameshanxinsun@gmail.com>
2020-07-09 20:40:00 -0400

    - Added a new round corner extension - Update shadow extension

---
+++ Sources/JimUtilitySDK/Extensions/UIView+addRoundCorner/UIView+addRoundCorner.swift
@@ -0,0 +1,23 @@
+//
+//  UIView+addRoundCorner.swift
+//  
+//
+//  Created by James Layton on 7/9/20.
+//
+
+#if os(Linux)
+// Code specific to Linux
+#elseif os(macOS)
+// Code specific to macOS
+#endif
+
+#if canImport(UIKit)
+import UIKit
+#endif
+
+extension UIView {
+    func addRoundCorner(radius: CGFloat, by: CGFloat, isClipsToBound: Bool = false) {
+        clipsToBounds = isClipsToBound
+        layer.cornerRadius = radius / by
+    }
+}
