662769ed1b4f0b5fb16d079142a9924db6a8bd82
James Layton <jameshanxinsun@gmail.com>
2023-01-21 00:49:16 -0500

    Fix Password Validation (#22)
    - Fix password validation issue with new regex
    - Add dynamic error message for requirements

--- Sources/JimUtilitySDK/Extensions/String/String+isValidPassword.swift
+++ Sources/JimUtilitySDK/Extensions/String/String+isValidPassword.swift
@@ -12,24 +12,55 @@ public extension String {
     /// Ensure is a valid password
     /// Least 8 characters, Least 1 Uppercase Alphabet, Least 1 Lowercase Alphabet, Least 1 numeric Number, Least 1 Special Character
     /// - Returns: A boolean indicate if the password meet the requirements
-    func isValidPassword() -> Bool {
-        let pwRegEx =
-            // Least 8 characters
-            #"(?=.{8,})"# +
+    func isValidPassword() -> String? {
 
-            // Least 1 Uppercase Alphabet
-            #"(?=.*[A-Z])"# +
-                
-            // Least 1 Lowercase Alphabet
-            #"(?=.*[a-z])"# +
-                
-            // Least 1 numeric Number
-            #"(?=.*\d)"# +
-                
-            // Least 1 Special Character
-            #"(?=.*[ !$%&?._-])"#
+        var errorMessage: String?
         
-        let pwResult = NSPredicate(format:"SELF MATCHES %@", pwRegEx)
-        return pwResult.evaluate(with: self)
+        if self.count < 8 {
+            if let errorMsg = errorMessage {
+                errorMessage = errorMsg + "• Minimum 8 characters\n"
+            } else {
+                errorMessage = "• Minimum 8 characters\n"
+            }
+        }
+        
+        if !checkRegex(with: self, regex: ".*[A-Z]+.*") {
+            if let errorMsg = errorMessage {
+                errorMessage = errorMsg + "• 1 Uppercase Alphabet\n"
+            } else {
+                errorMessage = "• 1 Uppercase Alphabet\n"
+            }
+        }
+        
+        if !checkRegex(with: self, regex: ".*[a-z]+.*") {
+            if let errorMsg = errorMessage {
+                errorMessage = errorMsg + "• 1 Lowercase Alphabet\n"
+            } else {
+                errorMessage = "• 1 Lowercase Alphabet\n"
+            }
+        }
+            
+        if !checkRegex(with: self, regex: ".*[0-9]+.*") {
+            if let errorMsg = errorMessage {
+                errorMessage = errorMsg + "• 1 Numeric Number\n"
+            } else {
+                errorMessage = "• 1 Numeric Number\n"
+            }
+        }
+        
+        if !checkRegex(with: self, regex: ".*[!&^%$#@()/]+.*") {
+            if let errorMsg = errorMessage {
+                errorMessage = errorMsg + "• 1 Special Character\n"
+            } else {
+                errorMessage = "• 1 Special Character\n"
+            }
+        }
+        
+        return errorMessage
+    }
+    
+    private func checkRegex(with source: String, regex: String) -> Bool {
+        let result = NSPredicate(format: "SELF MATCHES %@", regex)
+        return result.evaluate(with: self)
     }
 }
