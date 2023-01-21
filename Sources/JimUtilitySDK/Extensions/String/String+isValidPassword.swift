05eead38d1577231ba75cc88a654c56a287e824d
AsianJim <jameshanxinsun@gmail.com>
2023-01-20 20:52:03 -0500

    Update Password Regex - Replace password regex to include special characters

--- Sources/JimUtilitySDK/Extensions/String/String+isValidPassword.swift
+++ Sources/JimUtilitySDK/Extensions/String/String+isValidPassword.swift
@@ -10,10 +10,10 @@ import Foundation
 public extension String {
     
     /// Ensure is a valid password
-    /// Minimum 8 characters at least 1 Uppercase Alphabet, 1 Lowercase Alphabet and 1 Number
+    /// Minimum 8 characters at least 1 Uppercase Alphabet, 1 Lowercase Alphabet, 1 Number and 1 Special Character
     /// - Returns: A boolean indicate if the password meet the requirements
     func isValidPassword() -> Bool {
-        let pwRegEx = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$"
+        let pwRegEx = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[d$@$!%*?&#])[A-Za-z\\dd$@$!%*?&#]{8,}"
         let pwResult = NSPredicate(format:"SELF MATCHES %@", pwRegEx)
         return pwResult.evaluate(with: self)
     }
